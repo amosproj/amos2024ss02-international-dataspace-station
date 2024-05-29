4d805b10fc9847765489d9d0de7a22feea5ccb83
xenia1w <xenia.wysokinska@gmail.com>
2024-05-29 13:00:09 +0200

    cleanup of the code, added connector status for provider

---
+++ src/frontend/international-dataspace-station/app/dashboard/provider/connector_status.tsx
@@ -0,0 +1,38 @@
+import * as net from 'net';
+
+const PORT = 29191;
+
+function checkPortStatus(port: number): Promise<boolean> {
+  return new Promise((resolve) => {
+    const client = net.createConnection({ port }, () => {
+      client.end();
+      resolve(true);
+    });
+
+    client.on('error', () => {
+      resolve(false);
+    });
+  });
+}
+
+async function checkConnectorStatus(): Promise<string> {
+  try {
+    const isPortInUse = await checkPortStatus(PORT);
+    if (isPortInUse) {
+      console.log(`Connector is running on port ${PORT}`);
+      return("Your connector is running!");
+    } else {
+      console.log(`Connector is not running on port ${PORT}`);
+      return("Your connector is not running!");
+    }
+  } catch (error) {
+    console.error('Error occurred while checking connector status:', error);
+  }
+}
+
+export default function ConnectorStatus(){
+    const response = checkConnectorStatus();
+    return(
+    <p>response</p>
+    )
+}
