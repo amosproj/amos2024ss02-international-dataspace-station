a16b0be85a33d150c5061f2ed8851dc07c564518
skanatova <sezimkanatova23@gmail.com>
2024-06-04 00:31:20 +0200

    feat: minor changes on provider

--- src/frontend_socket/international-dataspace-station/app/dashboard/consumer/page.tsx
+++ src/frontend_socket/international-dataspace-station/app/dashboard/consumer/page.tsx
@@ -153,12 +153,9 @@ export default function Page() {
                 </div>
 
                 <div className="mt-6 flex items-center justify-start gap-x-6">
-                    <button type="button" className="text-sm font-semibold leading-6 text-gray-900">
-                        Cancel
-                    </button>
                     <button
                         type="submit"
-                        className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        className="px-4 py-2 rounded-md bg-neonGreen hover:bg-neonBlue text-white"
                     >
                         Save
                     </button>
--- src/frontend_socket/international-dataspace-station/app/dashboard/page.tsx
+++ src/frontend_socket/international-dataspace-station/app/dashboard/page.tsx
@@ -9,13 +9,13 @@ export default async function Page() {
             <div className="grid grid-cols-2 gap-5">
                 <div className="flex justify-evenly items-center bg-gray-100 border-gray-200 border-2 rounded-lg p-5">
                     <p>Consumer: {cons ? 'UP': 'DOWN'}</p>
-                    <button onClick={ () => {console.log('click')}} className={`rounded-lg px-5 py-2 ${cons ? 'bg-red-600' : 'bg-neonGreen'}`}>
+                    <button onClick={ () => {console.log('click')}} className={`px-4 py-2 rounded-md hover:bg-neonBlue text-white ${cons ? 'bg-red-600' : 'bg-neonGreen'}`}>
                         {cons ? 'Stop': 'Start'}
                     </button>
                 </div>
                 <div className="flex justify-evenly items-center bg-gray-100 border-gray-200 border-2 rounded-lg p-5">
                     <p>Provider: {prov ? 'UP': 'DOWN'}</p>
-                    <button onClick={ () => {console.log('click')}} className={`rounded-lg px-5 py-2 ${prov ? 'bg-red-600' : 'bg-neonGreen'}`}>
+                    <button onClick={ () => {console.log('click')}} className={`px-4 py-2 rounded-md hover:bg-neonBlue text-white ${prov ? 'bg-red-600' : 'bg-neonGreen'}`}>
                         {prov ? 'Stop': 'Start'}
                     </button>
                 </div>
