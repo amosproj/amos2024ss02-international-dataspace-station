d8b307aa5f8b6ff5bda8a48d85f777a494b90c96
esracosgun <esra.cosgun@gmx.de>
2024-06-03 22:14:11 +0200

    adjust button

--- src/frontend/international-dataspace-station/app/dashboard/page.tsx
+++ src/frontend/international-dataspace-station/app/dashboard/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 import { useState } from 'react';
+import './styles/style.css';
 
 export default function Page() {
   const [output, setOutput] = useState('');
@@ -25,7 +26,7 @@ export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
       <p>Dashboard Page</p>
-      <button onClick={handleClick}>Execute Command</button>
+      <button onClick={handleClick} className="custom-button">Execute Command</button>
       {output && <pre>{output}</pre>}  {/* Ausgabe auf der Seite anzeigen */}
       {connectionMessage && <p>{connectionMessage}</p>}  {/* Verbindungsstatus anzeigen */}
     </main>
