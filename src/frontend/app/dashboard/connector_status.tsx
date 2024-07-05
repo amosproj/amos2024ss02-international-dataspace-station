77683a4c8c3bb1843c297bd96c0319fb4862c8de
Daniel Kurtz <daku@mail.de>
2024-07-06 00:18:57 +0200

    Added database status to dashboard

--- src/frontend/app/dashboard/connector_status.tsx
+++ src/frontend/app/dashboard/connector_status.tsx
@@ -14,28 +14,56 @@ export default function ConnectorStatus({ connectorName }: ConnectorStatusProps)
     const [status, setStatus] = useState<string>('Checking connector status...');
     const [running, setRunning] = useState<boolean | null>(null);
 
+    const [dbStatus, setDbStatus] = useState<string>('Checking database status...');
+    const [dbRunning, setDbRunning] = useState<boolean | null>(null);
+
+    var connectorStatus: string;
+    var connectorRunning: boolean | null;
+    var databaseStatus: string;
+    var databaseRunning: boolean | null;
+
     const fetchStatus = async () => {
+        setStatus("Checking connector status...");
+        setRunning(null);
+        setDbStatus("Checking database status...");
+        setDbRunning(null);
+
         try {
-            setStatus("Checking connector status...");
-            setRunning(null);
             const response = await fetch(`/api/check_status?connector=${connectorName}`); 
             const data = await response.json();
-            console.log("STATUS: ", response.status);
-            console.log(data);
             if (data.status == 'running') {
-                setStatus(capitalizeFirstLetter(connectorName) + " connector is running!");
-                console.log(capitalizeFirstLetter(connectorName) + " connector is running!");
-                setRunning(true);
+                connectorStatus = "Connector is running!";
+                connectorRunning = true;
             } else {
-                setStatus(capitalizeFirstLetter(connectorName) + " connector is not running!");
-                console.log(capitalizeFirstLetter(connectorName) + " connector is not running!");
-                setRunning(false);
+                connectorStatus = "Connector is not running!";
+                connectorRunning = false;
             }
         } catch (error) {
             console.error('Error occurred while checking connector status:', error);
-            setStatus('Error occurred while checking connector status');
-            setRunning(null);
+            connectorStatus = 'Error occurred while checking connector status';
+            connectorRunning = null;
         }
+
+        try {
+            const response = await fetch(`/api/check_db_status?connector=${connectorName}`); 
+            const data = await response.json();
+            if (data.status == 'running') {
+                databaseStatus = "Database is running!";
+                databaseRunning = true;
+            } else {
+                databaseStatus = "Database is not running!";
+                databaseRunning = false;
+            }
+        } catch (error) {
+            console.error('Error occurred while checking database status:', error);
+            databaseStatus = 'Error occurred while checking database status';
+            databaseRunning = null;
+        }
+        setStatus(connectorStatus);
+        setRunning(connectorRunning);
+        setDbStatus(databaseStatus);
+        setDbRunning(databaseRunning);
+
     };
 
     useEffect(() => {
@@ -44,31 +72,46 @@ export default function ConnectorStatus({ connectorName }: ConnectorStatusProps)
 
     return (
         <div className={`flex items-center bg-gray-100 border-2 rounded-lg p-5 text-black ${
-            running === null 
+            running === null || dbRunning === null
             ? '' 
-            : running 
+            : running && dbRunning
             ? 'border-green-500' 
             : 'border-red-500'
         }`}>
             <div className="flex items-center space-x-4">
                 <button 
                     onClick={fetchStatus}
-                    className="px-4 py-2 text-black bg-white rounded hover:bg-neonBlue shadow-xl flex gap:1em"
+                    className="px-4 py-2 text-black bg-white rounded hover:bg-neonBlue shadow-xl flex"
                 >
                     <ArrowPathIcon className="w-6 mr-1"/>
                     Refresh
                 </button>
-                <div className="flex items-center">
-                    <span 
-                        className={`h-4 w-4 rounded-full ${
-                            running === null 
-                            ? 'bg-gray-500' 
-                            : running 
-                            ? 'bg-green-500' 
-                            : 'bg-red-500'
-                        }`} 
-                    />
-                    <span className="ml-2">{status}</span>
+                <div className="flex flex-col gap-1">
+                    <div className="flex items-center border-b-2 pb-1">
+                        <span 
+                            className={`h-4 w-4 rounded-full ${
+                                running === null 
+                                ? 'bg-gray-500' 
+                                : running 
+                                ? 'bg-green-500' 
+                                : 'bg-red-500'
+                            }`} 
+                        />
+                        <span className="ml-2">{status}</span>
+                    </div>
+
+                    <div className="flex items-center">
+                        <span 
+                            className={`h-4 w-4 rounded-full ${
+                                dbRunning === null 
+                                ? 'bg-gray-500' 
+                                : dbRunning 
+                                ? 'bg-green-500' 
+                                : 'bg-red-500'
+                            }`} 
+                        />
+                        <span className="ml-2">{dbStatus}</span>
+                    </div>
                 </div>
             </div>
         </div>
