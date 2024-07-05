7f8f2a4fe1ebb9ead6311e6a3e2bc844e9b496bb
Daniel Kurtz <daku@mail.de>
2024-07-05 02:50:48 +0200

    Enabled storage of large files

--- src/database/src/main/java/de/uni1/amos/filestorage/entity/FileEntity.java
+++ src/database/src/main/java/de/uni1/amos/filestorage/entity/FileEntity.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
 
 @Entity
 public class FileEntity {
@@ -12,6 +13,8 @@ public class FileEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String fileName;
+
+    @Lob
     private byte[] fileData;
 
     public Long getId() {
