3daabc16db5e9cba3c840ae76735efac91b201ed
Daniel Kurtz <daku@mail.de>
2024-07-16 12:02:02 +0200

    Added favicon

--- src/frontend/app/layout.tsx
+++ src/frontend/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,10 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-        <body className={inter.className}>{children}</body>
+            <Head>
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
+            <body className={inter.className}>{children}</body>
         </html>
     );
 }
